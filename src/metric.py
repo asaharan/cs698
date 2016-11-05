dfffcdb8f690a1599a1ef55d97a2492a1b903c1e
Aadi Yogakar <ayogakar@gmail.com>
2016-11-05 23:21:09 +0530

    metric for single graph

--- src/metric.py
+++ src/metric.py
@@ -2,25 +2,15 @@ from collections import defaultdict
 import graph as G
 
 #	In and Out Degree distribution
-def degreeDist(graph, sample):
-	originalDictIn = defaultdict(int)
-	originalDictOut = defaultdict(int)
-	sampleDictIn = defaultdict(int)
-	sampleDictOut = defaultdict(int)
+def degreeDist(graph):
+	inDict = defaultdict(int)
+	outDict = defaultdict(int)
 	for src, dest in G.getEdges(graph, weight=False):
-		originalDictIn[dest] += 1
-		originalDictOut[src] += 1
-	for src, dest in G.getEdges(sample, weight=False):
-		sampleDictIn[dest] += 1
-		sampleDictOut[src] += 1
-	originalIn = defaultdict(int)
-	originalOut = defaultdict(int)
-	sampleIn = defaultdict(int)
-	sampleOut = defaultdict(int)
+		inDict[dest] += 1
+		outDict[src] += 1
+	inDist = defaultdict(int)
+	outDist = defaultdict(int)
 	for node in G.getNodes(graph):
-		originalIn[originalDictIn[node]] += 1
-		originalOut[originalDictOut[node]] += 1
-	for node in G.getNodes(sample):
-		sampleIn[sampleDictIn[node]] += 1
-		sampleOut[sampleDictOut[node]] += 1
-	return originalIn, originalOut, sampleIn, sampleOut
+		inDist[inDict[node]] += 1
+		outDist[outDict[node]] += 1
+	return inDist, outDist
