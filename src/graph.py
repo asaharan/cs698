f6dba8e576fa7f41e91b19890ea3aba1356f9b2c
Aadi Yogakar <ayogakar@gmail.com>
2016-11-05 15:03:33 +0530

    modified functions to work with weighted directed graph

--- src/graph.py
+++ src/graph.py
@@ -1,23 +1,41 @@
 import random
 
-def generateGraph(nodes=100, edges=300, path=None):
+def getNeighbours(graph, src):
+	return [dest for dest, weight in graph[src]]
+
+def isNeighbour(graph, src, dest):
+	if dest in getNeighbours(graph, src):
+		return True
+	else:
+		return False
+
+def getEdgeWeight(graph, src, dest):
+	if not isNeighbour(graph, src, dest):
+		return None
+	else:
+		for nei, wt in graph[src]:
+			if nei == dest:
+				return wt
+
+def generateGraph(nodes=100, edges=300, weights=50, path=None):
 	graph = {}
 	i = 0
 	while i < edges:
 		src = random.randint(1, nodes)
 		dest = random.randint(1, nodes)
+		weight = random.randint(1, weights)
 		if src in graph.keys():
-			if dest not in graph[src]:
-				graph[src].add(dest)
+			if not isNeighbour(graph, src, dest):
+				graph[src].append((dest, weight))
 				i += 1
 		else:
-			graph[src] = set([dest])
+			graph[src] = [(dest, weight)]
 			i += 1
 	if path:
 		fp = open(path, 'w')
 		for src in graph.keys():
-			for dest in graph[src]:
-				fp.write(str(src) + " " + str(dest) + "\n")
+			for dest, weight in graph[src]:
+				fp.write(str(src) + " " + str(dest) + " " + str(weight) + "\n")
 		fp.close()
 	return graph
 
@@ -29,9 +47,10 @@ def readGraph(path):
 		data = line.split()
 		start = int(data[0])
 		dest = int(data[1])
+		weight = int(data[2])
 		if start in graph.keys():
-			graph[start].add(dest)
+			graph[start].append((dest, weight))
 		else:
-			graph[start] = set([dest])
+			graph[start] = [(dest, weight)]
 	fp.close()
 	return graph
