ca8d63b973da4758fd3762756073987cac8bfba8
asaharan <asaharan812@gmail.com>
2016-11-07 01:25:50 +0530

    deleteRandomNodes added

---
+++ src/deleteRandomNodes.py
@@ -0,0 +1,22 @@
+import graph as G
+import random
+def deleteRandomNodes(graph, resizeRatio=0.7):
+	sampleNodes = G.getNodes(graph)
+	sampleNodeCount = len(sampleNodes)
+	sampleGraph = graph.copy()
+	nodes = sampleNodeCount
+	while True:
+		print sampleNodeCount, nodes
+		if 1.0*sampleNodeCount/nodes <= resizeRatio:
+			break
+		sampleNodeCount -= 1
+		randomNode=random.choice(sampleNodes)
+		G.removeNode(sampleGraph, randomNode)
+	return sampleGraph
+
+if __name__=="__main__":
+	print "fetching data"
+	data = G.readGraph("../data/5000x25000.data")
+	print "running deletionOfRandomNode"
+	sample = deletionOfRandomNode(data, resizeRatio=0.7)
+	print "variables available globally: data, sample"
